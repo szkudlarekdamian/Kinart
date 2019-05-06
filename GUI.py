6ef621c6e21679e447c91f978e3905013780f0b1
margohanyz <margohanyz@users.noreply.github.com>
2019-05-06 09:04:45 +0200

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 import time
-import pyautogui
+#import pyautogui
+import handTracker as ht
+import cv2
 
 class Kinart(object):
 
@@ -12,29 +14,34 @@ class Kinart(object):
         self.root.lift() # always on the top
 
         # WINDOW SIZE
-        sheight, swidth = self.root.winfo_screenheight(), self.root.winfo_screenwidth()
-        self.root.geometry('{}x{}'.format(swidth,sheight))
+        self.sheight, self.swidth = self.root.winfo_screenheight(), self.root.winfo_screenwidth()
+        #self.root.geometry('{}x{}'.format(swidth,sheight))
+        #self.root.geometry('640x480+-8+-8')
+        #self.root.geometry('1536x801+-8+-8')
+
+        #self.frame = tk.Frame(self.root, bg='red')
+        #self.frame.pack(side="LEFT", fill='BOTH')
 
         # PEN BUTTON
-        self.pen_button = tk.Button(self.root, text='PEN', height=3, width=7, command=self.use_pen)
+        self.pen_button = tk.Button(frame, text='PEN', height=3, width=5, command=self.use_pen)
         self.pen_button.grid(row=0, column=0, sticky='NSEW')
 
         # ERASER BUTTON
-        self.eraser_button = tk.Button(self.root, text='eraser', height=3, width=7, command=self.use_eraser)
+        self.eraser_button = tk.Button(self.root, text='eraser', height=3, width=5, command=self.use_eraser)
         self.eraser_button.grid(row=0, column=1, sticky='NSEW')
 
         # 4 COLORS
-        self.color_button1 = tk.Button(self.root, bg='green', height=3, width=7, command=self.green_color)
+        self.color_button1 = tk.Button(self.root, bg='green', height=3, width=5, command=self.green_color)
         self.color_button1.grid(row=0, column=2, sticky='NSEW')
-        self.color_button2 = tk.Button(self.root, bg='red', height=3, width=7, command=self.red_color)
+        self.color_button2 = tk.Button(self.root, bg='red', height=3, width=5, command=self.red_color)
         self.color_button2.grid(row=0, column=3, sticky='NSEW')
-        self.color_button3 = tk.Button(self.root, bg='blue', height=3, width=7, command=self.blue_color)
+        self.color_button3 = tk.Button(self.root, bg='blue', height=3, width=5, command=self.blue_color)
         self.color_button3.grid(row=0, column=4, sticky='NSEW')
-        self.color_button4 = tk.Button(self.root, bg='black', height=3, width=7, command=self.black_color)
+        self.color_button4 = tk.Button(self.root, bg='black', height=3, width=5, command=self.black_color)
         self.color_button4.grid(row=0, column=5, sticky='NSEW')
 
         # PAINTING
-        self.painting = tk.Canvas(self.root, bg='white')
+        self.painting = tk.Canvas(self.root, bg='white', width=640, height=480)
         self.painting.grid(row=1, columnspan=6, sticky='NSEW')
 
         # AUTORESIZE ELEMENTS
@@ -47,30 +54,45 @@ class Kinart(object):
         self.root.grid_rowconfigure(0, weight=0)
         self.root.grid_rowconfigure(1, weight=1)
 
-        #bx, by = self.color_button1.event_generate(), self.color_button1.winfo_rooty()
-        #bw, bh = self.color_button1.winfo_width(), self.color_button1.winfo_height()
-        #print(str(bx) + ', ' + str(by))
-        #print(str(bw) + ', ' + str(bw))
-
+        self.root.update()
+        self.button1W = self.color_button1.winfo_width()
+        self.button1H = self.color_button1.winfo_height()
+        print(self.button1H)
+        print(self.button1W)
         self.setup()
 
-        while True:
-            for x in range(115,714):
-                pyautogui.moveTo(x,690)
-                pyautogui.mouseDown(button='left')
-                self.root.update()
+        #while True:
+        #    for x in lista:
+        #        time.sleep(0.1)
+        #        if self.old_x and self.old_y:
+        #            self.painting.create_line( self.old_x, self.old_y, x[0], x[1], width=self.line_width,
+        #                               fill=self.color, capstyle=tk.ROUND, smooth=tk.TRUE, splinesteps=36)
+        #        self.old_x = x[0]
+        #        self.old_y = x[1]
+        #    #pyautogui.moveTo(x, y)
+        #    #pyautogui.mouseDown(button='left')
+        #        self.root.update()
+        #    self.root.update()
 
-            #mx, my = pyautogui.position()
-            #bx, cy = self.color_button1.winfo_rootx(), self.color_button1.winfo_rooty()
-            #bw, bh = self.color_button1.winfo_width(), self.color_button1.winfo_height()
-            #self.root.update()
+        #self.root.mainloop()
 
-        self.root.mainloop()
+    def getWindowSize(self):
+        self.root.update()
+        return self.root.winfo_geometry()
+
+    def updateCoords(self, x, y):
+        time.sleep(0.1)
+        if self.old_x and self.old_y:
+            self.painting.create_line(self.old_x, self.old_y, x, y, width=self.line_width,
+                                      fill=self.color, capstyle=tk.ROUND, smooth=tk.TRUE, splinesteps=36)
+        self.old_x = x
+        self.old_y = y
+        self.root.update()
 
     def setup(self):
         self.ispainting = False
-        self.old_x = None
-        self.old_y = None
+        self.old_x = 0
+        self.old_y = 0
         self.line_width = 20
         self.color = self.DEFAULT_COLOR
         self.eraser_on = False
@@ -78,11 +100,17 @@ class Kinart(object):
         self.activate_button(self.active_button)
         self.active_button_color = self.color_button4
         self.activate_button_color(self.active_button_color)
-        self.painting.bind('<B1-Motion>', self.paint) # event format - the left button is being held down
-        self.painting.bind('<ButtonRelease-1>', self.reset)
+        #self.painting.bind('<B1-Motion>', self.paint) # event format - the left button is being held down
+        #self.painting.bind('<ButtonRelease-1>', self.reset)
 
     def use_pen(self):
         self.activate_button(self.pen_button)
+
+    def getscreenwidth(self):
+        return self.swidth
+
+    def getscreenheight(self):
+        return self.sheight
 
     def green_color(self):
         self.eraser_on = False
@@ -121,7 +149,7 @@ class Kinart(object):
         self.eraser_on = eraser_mode
         self.activate_button(self.pen_button)
 
-    def paint(self, event):
+    def paint(self,event):
         self.line_width = 20.0 if self.eraser_on else 5.0
         paint_color = 'white' if self.eraser_on else self.color
         if self.old_x and self.old_y:
@@ -130,14 +158,64 @@ class Kinart(object):
                                capstyle=tk.ROUND, smooth=tk.TRUE, splinesteps=36)
         self.old_x = event.x
         self.old_y = event.y
-        #print(str(self.old_x))
-        #print(str(x))
-        #print('The current pointer position is {0}'.format(position))
 
     def reset(self, event):
         self.old_x = None
         self.old_y = None
 
-
 if __name__ == '__main__':
-    Kinart()
+    #paint = Kinart()
+
+    # Ścieżka do filmu z mapą głębi lub ID kamery
+    videoPath = "C:\\Users\\marce\\Desktop\\videokinec_depth4_v2.avi"
+
+    # Obiekt klasy HandTracker, której głównym zadaniem jest zwracanie współrzędnych dłoni, na podstawie filmu mapy głębi
+    hT = ht.HandTracker(videoPath)
+    print(hT.width)
+    print(hT.height)
+
+    # Obiekt klasy Kinart - rysowanie
+    paint = Kinart()
+
+    while (hT.kinectOpened()):
+        # Sztuczne spowolnienie klatek, tylko do celów testowych
+        #time.sleep(0.1)
+        # Pobieranie kolejnej klatki
+        frame = hT.getNextFrame()
+        # Jeśli klatka została wczytana poprawnie to kontynuuj
+        if frame is not None:
+            # Jeśli dłoń nie została jeszcze zainicjalizowana do systemu
+            if hT.handInitialized is False:
+                # Pokaż klatkę z narysowaną przestrzenią na dłoń
+                cv2.imshow('Kinart', hT.getFrameWithInitBox(frame))
+                # Jeśli wciśnięto 'z' to rozpocznij inicjalizację dłoni
+                if cv2.waitKey(1) == ord('z'):
+                    hT.initTracker(frame)
+                    hT.handInitialized = True
+            # Jeśli dłoń została zainicjalizowana to
+            else:
+                # Śledź dłoń i uzyskaj jej współrzędne
+                frameWithCoords, coords = hT.trackHand(frame)
+                cv2.imshow('Kinart', frameWithCoords)
+                cv2.waitKey(1)
+
+                if coords != None:
+                    if coords[0] < 0:
+                        print()
+                    elif coords[0] > paint.getscreenwidth():
+                        print()
+                    elif coords[1] < 0:
+                        print()
+                    elif coords[1] > paint.getscreenheight():
+                        print()
+                    else:
+                        #print(coords)
+                        print(paint.getWindowSize())
+                        paint.updateCoords(coords[0],coords[1])
+                else:
+                    print("NONE coords")
+
+        else:
+            break
+
+    paint.root.mainloop()
