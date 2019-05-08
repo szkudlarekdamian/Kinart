8acb2c684ffcc75e9427af1e3cc4bf0af9462479
Damian Szkudlarek <38192105+szkudlarekdamian@users.noreply.github.com>
2019-05-08 14:11:02 +0200

    Added config file

---
+++ config.py
@@ -0,0 +1,14 @@
+checkingForHandInterval = 0.1
+howManyTimesHandMustBeFound = 8
+minimumValueToConsiderHand = 14
+maximumValueToConsiderHand = 17
+
+boundingBoxColorInit = (0,0,255)#(177, 187, 223)
+centerPointColorInit = (118, 100, 245)
+boundingBoxColorTracking = (0,255,0)#(173,245,145)
+centerPointColorTracking = (239,237,191)
+
+boundingBoxBorderWidth = 4
+
+centerValue = 0
+
