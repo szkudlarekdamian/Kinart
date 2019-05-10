fc03e362b2b4635f3169c782c9f494b0177a2eac
Damian Szkudlarek <38192105+szkudlarekdamian@users.noreply.github.com>
2019-05-10 21:38:54 +0200

    Added parameters explanation

--- config.py
+++ config.py
@@ -1,21 +1,50 @@
-#Hand initialization parameters
+# HAND INITIALIZATION
+# # Interwał (w sekundach), z jakim sprawdzana będzie obecność dłoni w InitBox
 CHECKING_FOR_HAND_INTERVAL = 1
+
+# # Ile razy z rzędu musi zostać znaleziona dłoń w InitBox (powiązane z interwałem powyżej)
 HOW_MANY_TIMES_HAND_MUST_BE_FOUND = 5
+
+# # Minimalna wartość odległości z sensorów Kinect, aby obiekt znalazł się w InitBox (im niższa wartość, tym bliżej
+# musimy być kamery)
 MINIMUM_VALUE_TO_CONSIDER_HAND = 14
+
+# # Maksymalna wartość odległości z sensorów Kinect, aby obiekt znalazł się w InitBox (im wyższa wartość, tym dalej
+# # musimy być kamery)
 MAXIMUM_VALUE_TO_CONSIDER_HAND = 17
+
+# # Bok kwadratowego wycinka obrazu, z którego zostaje obliczona mediana, przy inicjalizacji dłoni
 MEDIAN_SQUARE_SIDE = 30
 
-#Bounding boxes style parameters
+
+# BOUNDING BOXES STYLE
+# # Kolor kwadratu, wspomagającego inicjalizację dłoni - oznaczony jako InitBox
 BOUNDING_BOX_COLOR_INIT = (177, 187, 223)
+
+# # Kolor środka kwadratu, wspomagającego inicjalizację dłoni
 CENTER_POINT_COLOR_INIT = (118, 100, 245)
-BOUNDING_BOX_COLOR_TRACKING = (173,245,145)
-CENTER_POINT_COLOR_TRACKING = (239,237,191)
+
+# # Kolor kwadratu, wspomagającego wizualizację śledzenia dłoni - oznaczony jako TrackingBox
+BOUNDING_BOX_COLOR_TRACKING = (173, 245, 145)
+
+# # Kolor środka kwadratu, wspomagającego wizualizację śledzenia dłoni
+CENTER_POINT_COLOR_TRACKING = (239, 237, 191)
+
+# # Szerokość obramowania, InitBox i TrackingBox
 BOUNDING_BOX_BORDER_WIDTH = 4
 
-#Hand tracking parameters
+
+# HAND TRACKING PARAMETERS
+# # Maksymalna wartość odległości z sensorów Kinect, która nie jest usuwana z obrazu
 FURTHEST_DISTANCE = 31
+
+# # Minimalna wartość odległości z sensorów Kinect, która nie jest usuwana z obrazu
 CLOSEST_DISTANCE = 12
 
-#Visuals
+
+# VISUALS
+# # Czy InitBox ma być wyświetlany w trakcie śledzenia dłoni
 SHOW_INIT_BOX_DURING_TRACKING = True
+
+# # Czy pomocniczy kontur dłoni ma być wyświetlany w InitBox
 SHOW_INIT_HAND_CONTOUR = True
