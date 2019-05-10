73b28f905516fc42f064c48e26aca8c47d32e0f3
Damian Szkudlarek <38192105+szkudlarekdamian@users.noreply.github.com>
2019-05-10 16:30:34 +0200

    Added distance parameters

--- config.py
+++ config.py
@@ -1,14 +1,19 @@
+#Hand initialization parameters
 checkingForHandInterval = 0.1
-howManyTimesHandMustBeFound = 8
+howManyTimesHandMustBeFound = 6
 minimumValueToConsiderHand = 14
 maximumValueToConsiderHand = 17
 
-boundingBoxColorInit = (0,0,255)#(177, 187, 223)
+#Bounding boxes style parameters
+boundingBoxColorInit = (177, 187, 223)
 centerPointColorInit = (118, 100, 245)
-boundingBoxColorTracking = (0,255,0)#(173,245,145)
+boundingBoxColorTracking = (173,245,145)
 centerPointColorTracking = (239,237,191)
-
 boundingBoxBorderWidth = 4
 
-centerValue = 0
+#Hand tracking parameters
+furthestDistance = 31
+closestDistance = 12
 
+
+centerValue = 0
